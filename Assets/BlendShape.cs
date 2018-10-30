c550eab90e144c1397496340bdaf62232c50ba5e
jiangdi <957133710@qq.com>
2018-10-30 11:54:04 +0800

    first

---
+++ Assets/BlendShape.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter))]
+public class BlendShape : MonoBehaviour {
+    //表情基的Mesh网格
+    public Mesh[] meshs;
+    //每个表情基的权重值数组
+    public float[] weights;
+
+    bool isChange = false;
+
+    void Start() {
+        //初始化权重值数组
+        weights = new float[meshs.Length];
+        System.Array.Clear(weights, 0, weights.Length);
+        //将当前网格置为默认的模型,即第一个表情基模型
+        GetComponent<MeshFilter>().mesh = meshs[0];
+    }
+
+    //设置表情基的权重值
+    public void SetWeight(int argIndex, float argWeight) {
+        weights[argIndex] = (argWeight > 1 ? 1 : argWeight);
+        isChange = true;
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.W)) {
+            SetWeight(1, weights[1] + 0.1f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S)) {
+            SetWeight(1, weights[1] - 0.1f);
+        }
+        UpdateMesh();
+    }
+
+    //更新网格
+    void UpdateMesh() {
+#if UNITY_EDITOR
+#else
+        if (!isChange) return;
+#endif
+        //获取模型的网格信息
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        //初始化模型的顶点数组
+        Vector3[] vertices = new Vector3[meshs[0].vertices.Length];
+        System.Array.Copy(meshs[0].vertices, vertices, vertices.Length);
+
+        //根据权重值与表情基计算新的顶点位置
+        for (int w = 1; w < weights.Length; w++) {
+            if (weights[w] <= 0) continue;
+
+            //获取表情基顶点
+            Vector3[] verticesT = meshs[w].vertices;
+
+            int i = 0;
+            while (i < vertices.Length) {
+                vertices[i] = vertices[i] + (verticesT[i] - meshs[0].vertices[i]) * weights[w];
+                i++;
+            }
+        }
+        //改变顶点位置坐标
+        mesh.vertices = vertices;
+        isChange = false;
+    }
+}
---
+++ Assets/ComputeShaderCS.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(MeshFilter))]
+public class ComputeShaderCS : MonoBehaviour {
+    //表情基的Mesh网格 0为natural模型
+    public Mesh[] meshs;
+    //每个表情基的权重值数组(限制在0-1)
+    [Range(0,1)]
+    public float[] weights;
+    //计算着色器
+    public ComputeShader shader;
+
+    ComputeBuffer vertBuffer;       //顶点缓冲
+    ComputeBuffer blendshapeBuffer; //表情基模型缓冲
+    ComputeBuffer naturalBuffer;    //natural模型缓冲
+
+    //模型网格信息
+    Mesh mesh;
+
+    private void Start() {
+        //获取模型的网格信息
+        mesh = GetComponent<MeshFilter>().mesh;
+
+        //分配缓冲空间(每个float占4个字节，一个Vector3包括3个float,所占大小为4*3)
+        vertBuffer = new ComputeBuffer(meshs[0].vertices.Length, 4 * 3);
+        blendshapeBuffer = new ComputeBuffer(meshs[0].vertices.Length, 4 * 3);
+        naturalBuffer = new ComputeBuffer(meshs[0].vertices.Length, 4 * 3);
+    }
+
+    private void Update() {
+        //不必每一帧执行，此处为便于测试
+        RunShader();
+    }
+
+    //执行计算着色器
+    void RunShader() {
+
+        int kernel = shader.FindKernel("Cul");
+
+        Vector3[] vertices = new Vector3[meshs[0].vertices.Length];
+
+        //初始化缓冲
+        vertBuffer.SetData(meshs[0].vertices);
+        naturalBuffer.SetData(meshs[0].vertices);
+
+        //填充缓冲
+        shader.SetBuffer(kernel, "vertBuffer", vertBuffer);
+        shader.SetBuffer(kernel, "naturalBuffer", naturalBuffer);
+
+        //根据权重值与表情基计算新的顶点位置
+        for (int w = 1; w < weights.Length; w++) {
+
+            if (weights[w] <= 0) continue;
+
+            //初始化缓冲
+            blendshapeBuffer.SetData(meshs[w].vertices);
+
+            //填充缓冲
+            shader.SetBuffer(kernel, "blendshapeBuffer", blendshapeBuffer);
+            
+            //设置权重
+            shader.SetFloat("weight", weights[w]);
+
+            //执行Compute Shader
+            shader.Dispatch(kernel, 4, 4, 1);
+
+        }
+
+        //获取计算结果
+        vertBuffer.GetData(vertices);
+
+        //更改网格结构
+        mesh.vertices = vertices;
+    }
+
+    void OnDestroy() {
+        if (vertBuffer != null) vertBuffer.Release();
+        if (blendshapeBuffer != null) blendshapeBuffer.Release();
+        if (naturalBuffer != null) naturalBuffer.Release();
+    }
+}
